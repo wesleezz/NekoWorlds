5087f415dab166d36b478c874b7ff88421ed9815
Wesleezz <72977084+wesleezz@users.noreply.github.com>
2023-01-04 14:44:46 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,15 @@ export default function Home() {
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
+            >
+              By{' '}
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                className={styles.vercelLogo}
+                width={100}
+                height={24}
+                priority
               />
             </a>
           </div>
