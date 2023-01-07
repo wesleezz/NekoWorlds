822c99d39f039c83c11fefe75b516eaf2b3535d0
Wesleezz <72977084+wesleezz@users.noreply.github.com>
2023-01-07 23:04:56 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,9 +18,6 @@ export default function Home() {
           <div className={styles.description}>
           <div>
             <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
             >
             </a>
           </div>
