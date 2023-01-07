d14cc814965d407e8a928f6b278c0c9011a6fb46
Wesleezz <72977084+wesleezz@users.noreply.github.com>
2023-01-07 13:18:56 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,6 @@ export default function Home() {
       </Head>
     <main className={styles.main}>
           <div className={styles.description}>
-          <p>&nbsp;
-            <code className={styles.code}></code>
-          </p>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
