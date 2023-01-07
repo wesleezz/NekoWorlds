b005f3038329040cf7772e3f2063016e9adb3dc9
Wesleezz <72977084+wesleezz@users.noreply.github.com>
2023-01-07 13:17:42 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,11 @@ export default function Home() {
         <link rel="icon" href="/power.png" />
       </Head>
     <main className={styles.main}>
+          <div className={styles.description}>
+          <p>&nbsp;
+            <code className={styles.code}></code>
+          </p>
+          <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
