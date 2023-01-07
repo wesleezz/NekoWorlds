dc5adf20c1577410ce422dc83ee8e4b0c54bc09a
Wesleezz <72977084+wesleezz@users.noreply.github.com>
2023-01-07 20:38:24 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              {' '}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
