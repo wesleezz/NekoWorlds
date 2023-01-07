b630ba55d92d8e34300195928d1c24c1fcd0eb0e
Wesleezz <72977084+wesleezz@users.noreply.github.com>
2023-01-07 20:43:59 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,7 @@ export default function Home() {
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
+            >
             </a>
           </div>
         </div>
