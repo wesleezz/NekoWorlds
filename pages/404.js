05544aeb4f7c05b0763412e7ef46cbd85ebdd276
Wesleezz <72977084+wesleezz@users.noreply.github.com>
2023-01-08 09:59:39 +0100

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,18 @@
-// 404.js
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '../styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
 import Link from 'next/link'
 
 export default function FourOhFour() {
   return <>
-    <h1>404 - Page Not Found</h1>
+    <h1>404 - Страница не найдена</h1>
     <Link href="/">
       <a>
-        Go back home
+        Назад
       </a>
     </Link>
   </>
