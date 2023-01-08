027a45f6d59601f035df71e6ae9f5696ad7eedbf
Wesleezz <72977084+wesleezz@users.noreply.github.com>
2023-01-08 21:39:42 +0100

    Update rules.js

--- pages/rules.js
+++ pages/rules.js
@@ -1,3 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '../styles/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
 const Rules = () => {
  return <div>Правила</div>
  }
