f78afbfea87122dbea5d3992bb8f04c38a074f85
Wesleezz <72977084+wesleezz@users.noreply.github.com>
2023-01-09 13:54:01 +0100

    Update index.js

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,18 +1,2 @@
-const path = require('path');
-const express = require('express');
-
 const Auth = () => {
  export default Auth
-
-
-const app = express();
-
-app.use('',express.static(path.join(__dirname
-
-app.get('/', (request, response) => {
-	return response.sendFile('index.js'
-}
-
-app.get('/auth/discord', (request, response) => {
-	return response.sendFile('index.js'
-}
