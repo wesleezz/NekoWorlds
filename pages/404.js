668af2444332221ff333ef48aacbab6d40b643e8
Wesleezz <72977084+wesleezz@users.noreply.github.com>
2023-01-09 19:36:55 +0100

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,7 +8,8 @@ import Link from 'next/link'
 
 export default function FourOhFour() {
   return <>
-      <h1>404 - Страница не найдена</h1>
+         <h1>className={styles.center}>
+      404 - Страница не найдена</h1>
     <Link href="/">
       <a>
         Назад
