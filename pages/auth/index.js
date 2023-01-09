0c9f0ad0b093186f88ad55ad0f56b0a0fba30161
Wesleezz <72977084+wesleezz@users.noreply.github.com>
2023-01-09 13:50:16 +0100

    Create index.js

---
+++ pages/auth/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+
+const app = express();
+
+app.use('',express.static(path.join(__dirname, 'public')));
+
+app.get('/', (request, response) => {
+	return response.sendFile('index.html', { root: '.' });
+}
+
+app.get('/auth/discord', (request, response) => {
+	return response.sendFile('dashboard.html', { root: '.' });
+}
