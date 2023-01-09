ed4e5a7169e546851adddf128afd09a599ca0638
Wesleezz <72977084+wesleezz@users.noreply.github.com>
2023-01-09 19:33:52 +0100

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -9,7 +9,7 @@ import Link from 'next/link'
 
 export default function FourOhFour() {
   return <>
-     <h1 className={styles.center}>
+     <h1> className={styles.center}>
       404 - Страница не найдена</h1>
     <Link href="/">
       <a>
