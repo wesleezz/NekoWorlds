edfe9ce4de9ee6b7eb0813e567626393c51ef9c2
Wesleezz <lalalaca2011@gmail.com>
2023-01-14 21:06:30 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,40 @@
-import '../styles/globals.scss'
+import { MDXProvider } from '@mdx-js/react'
+import { useThemedStylesWithMdx } from '@theme-ui/mdx'
+import { ThemeProvider } from 'theme-ui'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const theme = {
+  colors: {
+    text: '#000',
+    background: '#fff',
+    primary: '#07c',
+  },
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'Georgia, serif',
+  },
+  fontWeights: {
+    heading: 700,
+  },
+  styles: {
+    h1: {
+      fontSize: 32,
+      fontFamily: 'heading',
+      fontWeight: 'heading',
+      color: 'primary',
+      mt: 4,
+      mb: 2,
+    },
+  },
+}
+
+function Provider({ children, theme, components }) {
+  const componentsWithStyles = useThemedStylesWithMdx(
+    useMDXComponents(components)
+  )
+
+  return (
+    <ThemeProvider theme={theme}>
+      <MDXProvider components={componentsWithStyles}>{children}</MDXProvider>
+    </ThemeProvider>
+  )
 }
