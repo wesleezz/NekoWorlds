581aa4c1f601b9de2a4af15b58cc5411698dd26f
Wesleezz <lalalaca2011@gmail.com>
2023-01-14 18:58:31 +0100

    Update rules.js

--- src/pages/rules.js
+++ src/pages/rules.js
@@ -5,6 +5,6 @@ import styles from '../styles/index.module.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 const Rules = () => {
-    return <h2 className={inter.className}> Правила <span>-&gt;</span></h2>
+    return <div className={styles.center}><h2 className={inter.className}> Правила </h2></div>
     }
     export default Rules
