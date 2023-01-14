28d6f67f8ef7207cb97dae3d8d269ba79ed46ba6
Wesleezz <lalalaca2011@gmail.com>
2023-01-14 20:25:08 +0100

    Create theme.config.tsx

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>NekoWorlds - Вики</span>,
+  project: {
+    link: 'https://github.com/',
+  },
+  chat: {
+    link: 'https://dsc.gg/nekocorp',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'NekoWorlds - Вики',
+  },
+}
+
+export default config
