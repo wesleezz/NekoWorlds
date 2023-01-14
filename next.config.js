5548ec54fdca82897e01703bbe916cf8c004094b
Wesleezz <lalalaca2011@gmail.com>
2023-01-14 21:24:09 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -15,12 +15,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
-  unstable_flexsearch: true,
-  unstable_staticImage: true,
-});
-
-module.exports = withNextra();
