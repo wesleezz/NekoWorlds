1c305dc9f150d5e25a8be1cdafebf1c8ef0ce31f
Wesleezz <lalalaca2011@gmail.com>
2023-01-14 22:12:28 +0100

    fff

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
 })
- 
-module.exports = withNextra()
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
