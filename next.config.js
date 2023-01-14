b4eb6aa540d661abc44eaab633640c06884ac294
Wesleezz <lalalaca2011@gmail.com>
2023-01-14 21:20:50 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,9 +9,11 @@ const withMDX = require('@next/mdx')({
 })
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  reactStrictMode: true,
-}
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+  unstable_flexsearch: true,
+  unstable_staticImage: true,
+});
 
-module.exports = withMDX(nextConfig)
+module.exports = withNextra();
