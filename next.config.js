497899ef1660bbaa6219ff0261ba99eb6b494769
Wesleezz <lalalaca2011@gmail.com>
2023-01-15 10:45:18 +0100

    tailwandcss

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'htm', 'html'],
   reactStrictMode: true,
 }
 
