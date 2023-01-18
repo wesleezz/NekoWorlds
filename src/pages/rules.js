f1a62ad2d2826b8dad3ebeff5695b08257fbb4c2
Wesleezz <lalalaca2011@gmail.com>
2023-01-18 19:48:18 +0100

    Update rules.js

--- src/pages/rules.js
+++ src/pages/rules.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { ChangeColorScheme } from '../components/change-color-scheme/change-color-scheme'
 import { Inter } from '@next/font/google'
 import styles from '../styles/index.module.scss'
 
