03e7d3c427474ff15e2771fc7bda6db420d725b2
Wesleezz <lalalaca2011@gmail.com>
2023-01-19 19:04:50 +0100

    ww

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -8,18 +8,21 @@ export default function Navigation() {
         <div className = {styles.navigation}>
             
             <Link href='/'>
-                <a><img src="/power-1.png" width="48" height="39"></img></a>
+                <a><image src="/power-1.png" width="48" height="39"></image></a>
             </Link>
 
             <Link href='/'>
-                <a>Сервера</a>
+                <a>Главная</a>
             </Link>
 
             <Link href='/about/'>
-                <a>О сайте</a>
+                <a>О сервере</a>
             </Link>
             
-            <a href="https://github.com/plasmoapp/minecraft-status" target="_blank" rel="noreferrer">GitHub</a>
+            <Link href='/rules/'>
+                <a>Правила</a>
+            </Link>
+                        <a href="https://github.com/plasmoapp/minecraft-status" target="_blank" rel="noreferrer">GitHub</a>
         </div>
     )
 }
