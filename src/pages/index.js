f35deb855b44ddc98c460a2effcb4be9d6eae04a
Wesleezz <lalalaca2011@gmail.com>
2023-01-19 19:07:21 +0100

    pochemy ono ne rabotaet

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/power-1.png" />
       </Head>
-
+      
       <Navigation></Navigation>
       
     <main className={styles.main}>
