94e283f134b16a440135b294186e73c334decdc8
Wesleezz <lalalaca2011@gmail.com>
2023-01-19 18:21:36 +0100

    Навигация

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.scss'
+import Navigation from '../components/navigation/navigation.js'
 
 const inter = Inter({ subsets: ['latin'] })
 
