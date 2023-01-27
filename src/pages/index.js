0cc8e8736a8fd30e36d4e8a5ccaa1e5403c18c5f
Wesleezz <lalalaca2011@gmail.com>
2023-01-27 18:11:48 +0100

    navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.scss'
-import navigation from '../components/navigation/bebravigation'
+import navigation from '../components/navigation/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
