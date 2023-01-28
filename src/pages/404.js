38c939af6d9b5a0438c47511ab5fd3bc350267bc
Wesleezz <lalalaca2011@gmail.com>
2023-01-28 14:01:08 +0100

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,45 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import { Inter } from '@next/font/google'
+import { Manrope } from '@next/font/google'
+import styles from '../styles/404.module.scss'
 
 const inter = Inter({ subsets: ['latin'] })
+const manrope = Manrope({ subsets: ['latin'] })
 
-import Link from 'next/link'
-
-export default function FourOhFour() {
-  return <>
-      <h1>404 - Страница не найдена</h1>
-    <Link href="/">
-      <a>
-        Назад
-      </a>
-    </Link>
-  </>
+export default function Page() {
+  return (
+    <>
+      <Head>
+        <title>Cтраница не найдена</title>
+        <meta name="description" content="Ванильный сервер" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/power-1.png" />
+      </Head>
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/magnifying_glass_tiled_left_3d.png"
+          alt="magnifying_glass_tiled_left_3d"
+          width={50}
+          height={50}
+          priority
+        />
+      </div>
+      <div className={styles.center}>
+      <div className={manrope.className}>
+        <h1>404</h1>
+      </div>
+      </div>
+      <div className={styles.center}>
+      <div className={inter.className}>
+        <h3>Мы искали везде но не смогли найти эту старницу</h3>
+      </div>
+      </div>
+      <Link
+        href="/">Назад
+      </Link>
+    </>
+ )
 }
