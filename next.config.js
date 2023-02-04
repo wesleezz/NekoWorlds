0f7d6be49f3f856cf13a69311604202013a3ba99
Wesleezz <lalalaca2011@gmail.com>
2023-02-04 15:39:16 +0100

    v

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'htm', 'html'],
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
   reactStrictMode: true,
 }
 
