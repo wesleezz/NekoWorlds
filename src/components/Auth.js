c504b02f720d1b955ac37dabc5d9320b02d0b864
Wesleezz <lalalaca2011@gmail.com>
2023-03-26 12:55:40 +0200

    🍀 Фикс

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -10,7 +10,7 @@ const manrope = Manrope({ subsets: ['latin'] })
 export default function Header() {
   return (
     <div className={styles.button}>
-      <a type="button" href="https://discord.com/api/oauth2/authorize?client_id=1001763225018318888&redirect_uri=https%3A%2F%2Fnekocorp.gq%2F&response_type=code&scope=identify"><Image src="/dsc.png" width="20" height="15" /> Discord</a>
+      <a type="button" href="https://discord.com/api/oauth2/authorize?client_id=1001763225018318888&redirect_uri=https%3A%2F%2Fnekocorp.gq%2F&response_type=code&scope=identify"><Image src="/dsc.png" alt="dsc" width="20" height="15" /> Discord</a>
     </div>
   )
 }
--- src/pages/auth.js
+++ src/pages/auth.js
@@ -18,12 +18,12 @@ export default function Page() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/power.png" />
       </Head>
-    <main className={styles.main}>
-      <Header></Header>
-      <div className={styles.center}>
-        <div className={inter.className}>
-          <div className={styles.button}>
-              <a type="button" href="https://discord.com/api/oauth2/authorize?client_id=1001763225018318888&redirect_uri=https%3A%2F%2Fnekocorp.gq%2F&response_type=code&scope=identify"><Image src="/dsc.png" width="20" height="15" /> Discord</a>
+      <main className={styles.main}>
+        <Header></Header>
+        <div className={styles.center}>
+          <div className={inter.className}>
+            <div className={styles.button}>
+              <a type="button" href="https://discord.com/api/oauth2/authorize?client_id=1001763225018318888&redirect_uri=https%3A%2F%2Fnekocorp.gq%2F&response_type=code&scope=identify"><Image src="/dsc.png" alt="dsc" width="20" height="15" /> Discord</a>
             </div>
           </div>
         </div>
